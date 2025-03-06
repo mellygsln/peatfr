38325cd94194fe151a00dc15893bf3cc5b534211
Melly <109222903+mellygsln@users.noreply.github.com>
2025-03-06 16:38:36 +0700

    Update Function Auto Predict ARIMA.R

--- R/Function Auto Predict ARIMA.R
+++ R/Function Auto Predict ARIMA.R
@@ -1,4 +1,9 @@
 autopredictarima <- function(WT, SM, Rf, Temp, h) {
+    if (!requireNamespace("forecast", quietly = TRUE)) {
+    install.packages("forecast")
+  }
+  library(forecast)
+
   data_combined <- data.frame(WT, SM, Rf, Temp)
   columns <- c("WT", "SM", "Rf", "Temp")
   models <- list()
